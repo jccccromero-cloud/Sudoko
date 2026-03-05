8146eedc9b89fbb74cd54c46baa6409bb1354534
Jericho-args <vilocura1990@gmail.com>
2026-03-05 10:18:29 +0800

    Vince Esgana commit

--- src/GameTimer.java
+++ src/GameTimer.java
@@ -1,4 +1,17 @@
 
 public class GameTimer {
+  private long startTime;
+    private long endTime;
 
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedSeconds() {
+        return (endTime - startTime) / 1000;
 }
+
