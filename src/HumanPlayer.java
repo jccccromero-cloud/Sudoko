cb994c558226c25207987274c4ece02afc7f2165
Jericho-args <vilocura1990@gmail.com>
2026-03-05 10:19:08 +0800

    Vince Esgana Commit HumanPlayer

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,4 +1,11 @@
+class HumanPlayer extends Player {
 
-public class HumanPlayer {
+    public HumanPlayer(String name) {
+        super(name);
+    }
 
+    @Override
+    public void makeMove(SudokuBoard board, Move move) {
+        board.placeNumber(move.getRow(), move.getCol(), move.getValue());
+    }
 }
